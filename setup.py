b9712b76b44462a8bbe56599dcb023d6d9490bac
roondar <roondar56@gmail.com>
2017-03-26 12:59:19 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name='RFLink-update',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='roondar',
+    author_email='',
+    description='',
+    executables=[Executable("update_RFLink.py")],
+)
